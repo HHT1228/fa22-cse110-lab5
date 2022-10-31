3b2c01a99cb3b6f5e060a93afdcea0d0e259c6c5
h4henry <97231532+h4henry@users.noreply.github.com>
2022-10-30 17:16:33 -0700

    expose.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,80 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  const synth = window.speechSynthesis;
+
+  const voiceType = document.getElementById("voice-select");
+  const voiceImage = document.querySelector("[alt='Smiling face']");
+  const textInput = document.getElementById("text-to-speak");
+  const playButton = document.querySelector("button");
+  //init voices array
+  let voices = [];
+  const getVoices = () => {
+    voices = synth.getVoices();
+  
+    // Loop through voices and create an option for each one
+    voices.forEach(voice => {
+      // Create option element
+      const option = document.createElement('option');
+      // Fill option with voice and language
+      option.textContent = voice.name + '(' + voice.lang + ')';
+  
+      // Set needed option attributes
+      option.setAttribute('data-lang', voice.lang);
+      option.setAttribute('data-name', voice.name);
+      voiceType.appendChild(option);
+    });
+  };
+
+  getVoices();
+  if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices;
+  }
+
+
+
+  //speak
+  const speak = () => {
+    // Check if speaking
+    if (synth.speaking) {
+      console.error('Already speaking...');
+      return;
+    }
+    if (textInput.value !== '') {
+  
+      voiceImage.src = "assets/images/smiling-open.png"
+      // Get speak text
+      const speakText = new SpeechSynthesisUtterance(textInput.value);
+  
+      // Speak end
+      speakText.onend = e => {
+        console.log('Done speaking...');
+        voiceImage.src = "assets/images/smiling.png"
+      };
+  
+
+      // Selected voice
+      const selectedVoice = voiceType.selectedOptions[0].getAttribute(
+        'data-name'
+      );
+  
+      // Loop through voices
+      voices.forEach(voice => {
+        if (voice.name === selectedVoice) {
+          speakText.voice = voice;
+        }
+      });
+  
+      // Speak
+      synth.speak(speakText);
+    }
+  };
+
+
+  playButton.addEventListener("click", (event) => {
+    speak();
+  })
+
+
 }
